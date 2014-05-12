483e04671095eedabc8972982dd2109a5329c603
Audrey Roy <audreyr@gmail.com>
2014-05-12 14:14:06 -0700

    Fix tests to match updated defs.

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -10,13 +10,13 @@ Tests for `columns.templatetags` module.
 
 import unittest
 
-from columns.templatetags.columns import rows, columns
+from columns.templatetags.columns import columns
 
 
 class TestColumns(unittest.TestCase):
 
     def test_columns(self):
         data = range(7)
-        result = rows(data, 2)
+        result = columns(data, 2)
         expected = [[0, 1, 2, 3], [4, 5, 6]]
         self.assertEqual(result, expected)
