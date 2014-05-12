d581761c876401ceacd4c49a815b0689b11c92f4
Audrey Roy <audreyr@gmail.com>
2014-05-12 13:30:44 -0700

    Rename to be more intuitive to me. Change example to show it working with Bootstrap grid.

--- columns/templatetags/columns.py
+++ columns/templatetags/columns.py
@@ -1,18 +1,19 @@
 """
-Template filters to partition lists into rows or columns.
+Template filters to partition lists into columns.
 
-A common use-case is for splitting a list into a table with columns::
+A common use-case is for splitting a list into 2 lists, to fill 2 columns::
 
-    {% load partition %}
-    <table>
-    {% for row in mylist|columns:3 %}
-        <tr>
-        {% for item in row %}
-            <td>{{ item }}</td>
+    {% load columns %}
+
+    <div class="row">
+        {% for col in mylist|columns:3 %}
+            <div class="col-md-4">
+                {% for item in col %}
+                    <div class="item">{{ item }}</div>
+                {% endfor %}
+            </div><!-- /col-md-4 -->
         {% endfor %}
-        </tr>
-    {% endfor %}
-    </table>
+    </div><!-- /row -->
 
 Code adapted from https://djangosnippets.org/snippets/401/ 
 
@@ -22,30 +23,30 @@ from django.template import Library
 
 register = Library()
 
-def rows(thelist, n):
+def columns(thelist, n):
     """
-    Break a list into ``n`` rows, filling up each row to the maximum equal
+    Break a list into ``n`` columns, filling up each row to the maximum equal
     length possible. For example::
 
         >>> l = range(10)
 
-        >>> rows(l, 2)
+        >>> columns(l, 2)
         [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
 
-        >>> rows(l, 3)
+        >>> columns(l, 3)
         [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9]]
 
-        >>> rows(l, 4)
+        >>> columns(l, 4)
         [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
 
-        >>> rows(l, 5)
+        >>> columns(l, 5)
         [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
 
-        >>> rows(l, 9)
+        >>> columns(l, 9)
         [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9], [], [], [], []]
 
-        # This filter will always return `n` rows, even if some are empty:
-        >>> rows(range(2), 3)
+        # This filter will always return `n` columns, even if some are empty:
+        >>> columns(range(2), 3)
         [[0], [1], []]
     """
     try:
@@ -60,105 +61,5 @@ def rows(thelist, n):
         split += 1
     return [thelist[split*i:split*(i+1)] for i in range(n)]
 
-def rows_distributed(thelist, n):
-    """
-    Break a list into ``n`` rows, distributing columns as evenly as possible
-    across the rows. For example::
 
-        >>> l = range(10)
-
-        >>> rows_distributed(l, 2)
-        [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
-
-        >>> rows_distributed(l, 3)
-        [[0, 1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-        >>> rows_distributed(l, 4)
-        [[0, 1, 2], [3, 4, 5], [6, 7], [8, 9]]
-
-        >>> rows_distributed(l, 5)
-        [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
-
-        >>> rows_distributed(l, 9)
-        [[0, 1], [2], [3], [4], [5], [6], [7], [8], [9]]
-
-        # This filter will always return `n` rows, even if some are empty:
-        >>> rows(range(2), 3)
-        [[0], [1], []]
-    """
-    try:
-        n = int(n)
-        thelist = list(thelist)
-    except (ValueError, TypeError):
-        return [thelist]
-    list_len = len(thelist)
-    split = list_len // n
-
-    remainder = list_len % n
-    offset = 0
-    rows = []
-    for i in range(n):
-        if remainder:
-            start, end = (split+1)*i, (split+1)*(i+1)
-        else:
-            start, end = split*i+offset, split*(i+1)+offset
-        rows.append(thelist[start:end])
-        if remainder:
-            remainder -= 1
-            offset += 1
-    return rows
-
-def columns(thelist, n):
-    """
-    Break a list into ``n`` columns, filling up each column to the maximum equal
-    length possible. For example::
-
-        >>> from pprint import pprint
-        >>> for i in range(7, 11):
-        ...     print '%sx%s:' % (i, 3)
-        ...     pprint(columns(range(i), 3), width=20)
-        7x3:
-        [[0, 3, 6],
-         [1, 4],
-         [2, 5]]
-        8x3:
-        [[0, 3, 6],
-         [1, 4, 7],
-         [2, 5]]
-        9x3:
-        [[0, 3, 6],
-         [1, 4, 7],
-         [2, 5, 8]]
-        10x3:
-        [[0, 4, 8],
-         [1, 5, 9],
-         [2, 6],
-         [3, 7]]
-
-        # Note that this filter does not guarantee that `n` columns will be
-        # present:
-        >>> pprint(columns(range(4), 3), width=10)
-        [[0, 2],
-         [1, 3]]
-    """
-    try:
-        n = int(n)
-        thelist = list(thelist)
-    except (ValueError, TypeError):
-        return [thelist]
-    list_len = len(thelist)
-    split = list_len // n
-    if list_len % n != 0:
-        split += 1
-    return [thelist[i::split] for i in range(split)]
-
-register.filter(rows)
-register.filter(rows_distributed)
 register.filter(columns)
-
-def _test():
-    import doctest
-    doctest.testmod()
-
-if __name__ == "__main__":
-    _test()
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -15,20 +15,8 @@ from columns.templatetags.columns import rows, columns
 
 class TestColumns(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    def test_rows(self):
+    def test_columns(self):
         data = range(7)
         result = rows(data, 2)
         expected = [[0, 1, 2, 3], [4, 5, 6]]
         self.assertEqual(result, expected)
-
-    def test_columns(self):
-        data = range(7)
-        result = columns(data, 2)
-        expected = [[0, 4], [1, 5], [2, 6], [3]]
-        self.assertEqual(result, expected)
-
-    def tearDown(self):
-        pass
